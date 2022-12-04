eef279042152ec4341d8a613d686cc29123b7321
yskfukui <1403yfukui@gmail.com>
2022-12-04 17:24:40 +0900

    ADD:vuetifyを導入

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,30 @@
 import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
-<template>
+<!-- <template>
   <main>
     <TheWelcome />
   </main>
+</template> -->
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <TheWelcome />
+    </v-main>
+  </v-app>
 </template>
+
+<script lang="ts">
+export default {
+  data: () => ({ drawer: true }),
+}
+</script>
